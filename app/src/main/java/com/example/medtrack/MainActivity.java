ab0a8e08e13428b06dc789ff8202e2f9f1565aec
Aaliyah Sealey <aaliyahs9023@hstat.org>
2020-04-05 20:15:58 +0000

    commented out error

--- app/src/main/java/com/example/medtrack/MainActivity.java
+++ app/src/main/java/com/example/medtrack/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent1 = new Intent(MainActivity.this, AlarmReceiver.class);
                 PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0,intent1, PendingIntent.FLAG_UPDATE_CURRENT);
                 AlarmManager am = (AlarmManager) MainActivity.this.getSystemService(MainActivity.this.ALARM_SERVICE);
-                am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+                /*am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);*/
 
                 alarm.set(AlarmManager.RTC_WAKEUP, alarmStartTime, alarmIntent);
 
